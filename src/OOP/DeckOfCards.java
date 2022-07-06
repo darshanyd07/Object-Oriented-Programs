59b15927d953e254e15876953a4fe154e3523872
champ09D <darshanmhatre709@gmail.com>
2022-07-07 00:27:39 +0530

    Deck-Of-Cards file done

--- src/OOP/DeckOfCards.java
+++ src/OOP/DeckOfCards.java
@@ -1,68 +1,63 @@
 package OOP;
+import java.util.HashSet;
 import java.util.Scanner;
-public class Account
-{
-    private double balance;
-    public Account ( double initialBalance )
-        {
-            if ( initialBalance > 0.0 )
-                balance = initialBalance;
-        }
+import java.util.Set;
 
+public class DeckOfCards {
 
-        public void credit( double amount )
+    public static void main(String[] args) {
+        String[][] cardCombo = new String[4][13];
+        String[][] player = new String[4][9];
+        Set<String> cards  = new HashSet<String>();
+        int a = 0, b = 0, c = 0, d = 0, num = 1;
+
+        String[] suits  = {"Diamond", "Heart", "Spade", "Club"};
+        String[] rank  = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen","King", "Ace" };
+
+        for(int i = 0; i < 4; i++)
         {
-            balance = balance + amount;	// add amount to balance
-        }
-        public void debit( double debitAmount )
-        {
-            if (debitAmount > balance)
+            for(int j = 0; j < 13; j++)
             {
-                debitAmount = 0.0;
-                System.out.println( "Debit amount exceeded account balance." );
+                cardCombo[i][j] = suits[i] + rank[j];
             }
-            balance = balance - debitAmount;
         }
 
+        while(num <=36){
+            int randomSuit = (int) Math.floor(Math.random()*10) % 4;
+            int randomNum = (int) Math.floor(Math.random()*100) % 13;
 
-        public double getBalance()
+
+            if (cards.add(cardCombo[randomSuit][randomNum]))
+            {
+                int option = num % 4;
+                switch (option)
+                {
+                    case 0:
+                        player[option][a++] = cardCombo[randomSuit][randomNum];
+                        break;
+                    case 1:
+                        player[option][b++] = cardCombo[randomSuit][randomNum];
+                        break;
+                    case 2:
+                        player[option][c++] = cardCombo[randomSuit][randomNum];
+                        break;
+                    case 3:
+                        player[option][d++] = cardCombo[randomSuit][randomNum];
+                        break;
+                }
+                num++;
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
         {
-            return balance;
+            System.out.print("Player :-" + (i+1) + " : ");
+            for (int j=0; j < 9; j++)
+            {
+                System.out.print(" "+player[i][j] + " ");
+            }
+            System.out.println();
         }
 
-    public static void main(String[] darsh)
-    {
-        Account account1 = new Account( 50.00 );
-        Account account2 = new Account( 70.53 );
-
-        System.out.println("------------ Welcome In Darsh Bank -------------");
-        System.out.printf( "account1 balance: $%.2f\n", account1.getBalance() );
-        System.out.printf( "account2 balance: $%.2f\n\n", account2.getBalance() );
-
-
-        Scanner input = new Scanner ( System.in );
-        double withdrawAmount;
-
-        System.out.print( "Enter debit amount for account1: " );
-        withdrawAmount = input.nextDouble();
-
-        System.out.printf("\ndeducting %.2f from account1 balance\n\n", withdrawAmount );
-        account1.debit(withdrawAmount); // deduct from account1 balance
-
-
-        System.out.printf( "account1 balance: $%.2f\n", account1.getBalance() );
-        System.out.printf( "account2 balance: $%.2f\n\n", account2.getBalance() );
-
-        System.out.print( "Enter withdraw amount for account2: "); // prompt
-        withdrawAmount = input.nextDouble(); // obtain user input
-
-        System.out.printf( "\ndeducting %.2f from account2 balance\n\n", withdrawAmount );
-        account2.debit( withdrawAmount ); //deduct from account2 balance
-
-
-        System.out.printf( "Account1 Balance: $%.2f\n", account1.getBalance() );
-        System.out.printf( "Account2 Balance: $%.2f\n", account2.getBalance() );
-        System.out.println("----------------------------");
     }
 }
-
